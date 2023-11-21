3050a3e39ca2217a703e4adc86515713bc33102d
mahedi205339 <mahedi205339gmaol.com>
2023-11-22 00:33:10 +0600

    get payment route according to email

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-console.log(process.env.STRIPE_SECRET_KEY)
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -36,6 +35,7 @@ async function run() {
         const reviewsCollection = client.db("bossDb").collection("reviews")
         const cartsCollection = client.db("bossDb").collection("carts")
         const usersCollection = client.db("bossDb").collection("users")
+        const paymentsCollection = client.db("bossDb").collection("payments")
 
 
         // jwt related  api 
@@ -263,39 +263,59 @@ async function run() {
             }
         })
 
-        // payment intent 
-        // app.post('/create-payment-intent', async (req, res) => {
-        //     try {
-        //         const { price } = req.body;
-        //         const amount = parseInt(price * 100);
-        //         const paymentIntent = await stripe.paymentIntents.create({
-        //             amount: amount,
-        //             currency:"usd",
-        //             payment_method_types: ['card'],
-        //         })
-        //         res.send({
-        //             clientSecret:paymentIntent.client_secret
-        //         })
-        //     } catch {
-        //         error => console.log(error)
-        //     }
-        // })
+
         app.post('/create-payment-intent', async (req, res) => {
             const { price } = req.body;
             const amount = parseInt(price * 100);
             console.log(amount, 'amount inside the intent')
-      
+
             const paymentIntent = await stripe.paymentIntents.create({
-              amount: amount,
-              currency: 'usd',
-              payment_method_types: ['card']
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
             });
-      
+
             res.send({
-              clientSecret: paymentIntent.client_secret
+                clientSecret: paymentIntent.client_secret
             })
-          });
-      
+        });
+
+        //payment related api 
+        app.post('/payments', async (req, res) => {
+            try {
+                const payment = req.body;
+                const paymentResult = await paymentsCollection.insertOne(payment)
+                console.log('payment info', payment)
+                //carefully delete each item from cart
+                const query = {
+                    _id: {
+                        $in: payment.cartIds.map(id => new ObjectId(id))
+                    }
+                }
+                const deleteResult = await cartsCollection.deleteMany(query)
+                res.send({ paymentResult, deleteResult })
+            }
+            catch {
+                error => console.log(error)
+            }
+        })
+
+        app.get('/payments/:email', verifyToken, async (req, res) => {
+            try {
+                const query = { email: req.params.email }
+                if (req.params.email !== req.decoded.email) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
+                const result = await paymentsCollection.find(query).toArray();
+                res.send(result)
+            }
+            catch {
+                error => console.log(error)
+            }
+        })
+
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
